8ba058d3e051fcb1b5ed8036e89c9f2390303ef7
Egorka-vader <egorm3075@gmail.com>
2026-05-08 20:19:04 +0300

    Update keyboard.py

--- app/keyboard.py
+++ app/keyboard.py
@@ -2,5 +2,5 @@ from aiogram.types import InlineKeyboardButton,InlineKeyboardMarkup,ReplyKeyboar
 
 start_keyboard = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='📋Лист задач',callback_data='task_list'),InlineKeyboardButton(text='👤Профиль',callback_data='profile')],
-    [InlineKeyboardButton(text='➕ Добавить задачу',callback_data='add_task'),InlineKeyboardButton(text='🔑 Подписка',callback_data='premium')]
+    [InlineKeyboardButton(text='➕ Добавить задачу',callback_data='add_task'),InlineKeyboardButton(text='💳 Подписка',callback_data='premium')]
 ])
